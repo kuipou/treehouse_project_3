1f3297250f28c984c3251424b341c94e43fdadce
kuipou <kuipou@gmail.com>
2018-03-14 12:33:35 -0400

    started controllers

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-const init = () => {
+const FormController = (function () {
+})();
 
-};
+const UIController = (function () {
+  const DOMStrings = {
+    
+  };
+  return {
+    getDOMStrings: () => DOMstrings
+  };
+})();
+
+const controller = (function (formCtrl, UICtrl) {
+  return {
+    init : () => {
+      console.log('Application has started.');
+
+      // ## Set focus on the first text field
+      // * When the page loads, give focus to the first text field
+
+      document.addEventListener("DOMContentLoaded", event => {
+
+      })
+    }
+  };
+})(FormController, UIController);
+
+
+controller.init();
