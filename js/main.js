b357cb7c2651719c4aa8722e3f6b64c51ad78134
kuipou <kuipou@gmail.com>
2018-03-15 15:29:17 -0400

    got focus on first item

--- js/main.js
+++ js/main.js
@@ -2,25 +2,36 @@ const FormController = (function () {
 })();
 
 const UIController = (function () {
-  const DOMStrings = {
-    
+  const DOMstrings = {
+    inputName: "#name",
+    inputMail: "#mail",
+    selectTitle : "#title",
+    selectSize : "#size"
   };
   return {
-    getDOMStrings: () => DOMstrings
+    getDOMstrings: function () {
+      return DOMstrings;
+    }
   };
 })();
 
 const controller = (function (formCtrl, UICtrl) {
+  const setupEventListeners = () => {
+    const DOM = UICtrl.getDOMstrings();
+    // setup the onload event
+    document.addEventListener("DOMContentLoaded", event => {
+      document.querySelector(DOM.inputName).focus();
+      console.log("Focus is set on first name " + DOM.inputName);
+    });
+  };
   return {
     init : () => {
       console.log('Application has started.');
 
       // ## Set focus on the first text field
       // * When the page loads, give focus to the first text field
-
-      document.addEventListener("DOMContentLoaded", event => {
-
-      })
+      setupEventListeners();
+      
     }
   };
 })(FormController, UIController);
